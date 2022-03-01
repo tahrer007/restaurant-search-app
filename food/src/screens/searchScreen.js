091ed3460425a3b909221eef0539b9ac7eb75082
tahrer ad <tahrer_Ad@outlook.com>
2022-03-01 09:46:29 +0200

    search api function

--- food/src/screens/searchScreen.js
+++ food/src/screens/searchScreen.js
@@ -1,19 +1,42 @@
-import React ,{useState} from "react";
-import { View , Text ,StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/searchBar";
+import yelp from "../api/yelp";
 
-const SearchScreen =()=>{
-    const onTermSubmit = ()=> console.log("submit !!!")
-    const [term,setTerm]=useState("");
-    return <View>
-       < SearchBar 
-       term={term} 
-       onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={onTermSubmit}
-       />
-       
+const SearchScreen = () => {
+  const [results, setResults] = useState([]);
+  const [term, setTerm] = useState("");
+  const [errorMsg ,setErrorMsg]=useState("")
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (error) {
+        setErrorMsg("something is wrong , try again later ");
+    }
+  };
+
+ 
+  return (
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm)=>setTerm(newTerm)}
+        onTermSubmit={searchApi}
+      />
+      <Text> {term}</Text>
+      <Text> {results.length}</Text>
+      {errorMsg ?   <Text> {errorMsg}</Text> : null }
+    
     </View>
-}
+  );
+};
 
 const styles = StyleSheet.create({});
-export default SearchScreen ; 
+export default SearchScreen;
